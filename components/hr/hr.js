bacc21d4a5565d35523d3dab566ac3c4b3cb04e5
BossBele <adambeleko@gmail.com>
2021-05-07 17:17:58 +0300

    sty;es

--- components/hr/hr.js
+++ components/hr/hr.js
@@ -1,17 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styled from "@emotion/styled";
+
+const styles = styled.hr`
+  display: "inline-block",
+  marginBlock: auto,
+  marginInline: 0.5em,
+  height: 50px,
+`;
 
 export default function hr({ vertical = false, ...props }) {
-  const styles = {
-    vertical: {
-      display: "inline-block",
-      marginBlockStart: "auto",
-      marginBlockEnd: "auto",
-      marginInline: "0.5em",
-      height: 50,
-    },
-  };
-  return <hr {...props} style={styles.vertical} />;
+  // const styles = {
+  //   vertical: {
+  //     display: "inline-block",
+  //     marginBlockStart: "auto",
+  //     marginBlockEnd: "auto",
+  //     marginInline: "0.5em",
+  //     height: 50,
+  //   },
+  // };
+  return <hr {...props} />;
 }
 
 hr.propTypes = {
