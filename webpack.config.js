bd4cffff3f055289093f9ad9d2c437a711263391
BossBele <adambeleko@gmail.com>
2021-05-07 18:48:01 +0300

    config

--- webpack.config.js
+++ webpack.config.js
@@ -8,18 +8,18 @@ module.exports = {
   // Enable sourcemaps for debugging webpack"s output.
   //   devtool: "source-map",
 
-  //   resolve: {
-  //     // Add ".ts" and ".tsx" as resolvable extensions.
-  //     extensions: [".ts", ".tsx", ".js", ".json"],
-  //   },
+  resolve: {
+    // Add ".ts" and ".tsx" as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js", ".json"],
+  },
 
   module: {
     rules: [
       // All files with a ".ts" or ".tsx" extension will be handled by "awesome-typescript-loader".
       { test: /\.tsx?$/, exclude: /node_modules/, loader: "ts-loader" },
 
-      //   // All output ".js" files will have any sourcemaps re-processed by "source-map-loader".
-      //   { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+      // All output ".js" files will have any sourcemaps re-processed by "source-map-loader".
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
 
       //   { test: /\.css$/, use: ["style-loader", "css-loader"] },
     ],
