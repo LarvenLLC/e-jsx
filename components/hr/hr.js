306a0b59110389bc959dfc3d815e642e87eed075
BossBele <adambeleko@gmail.com>
2021-05-07 16:49:38 +0300

    vertical

--- components/hr/hr.js
+++ components/hr/hr.js
@@ -1,10 +1,10 @@
 import React from "react";
-// import PropTypes from "prop-types";
+import PropTypes from "prop-types";
 
-export default function hr({ horizontal = false, ...props }) {
-  return <hr horizontal={horizontal} {...props} />;
+export default function hr({ vertical = false, ...props }) {
+  return <hr vertical={vertical} {...props} />;
 }
 
-// MyComponent.propTypes = {
-//   horizontal: PropTypes.bool,
-// };
+hr.propTypes = {
+  vertical: PropTypes.bool,
+};
