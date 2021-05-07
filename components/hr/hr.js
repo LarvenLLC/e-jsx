436fd07792025b1c2d16d46a31c222c925422583
BossBele <adambeleko@gmail.com>
2021-05-07 17:09:28 +0300

    hr

--- components/hr/hr.js
+++ components/hr/hr.js
@@ -2,7 +2,16 @@ import React from "react";
 import PropTypes from "prop-types";
 
 export default function hr({ vertical = false, ...props }) {
-  return <hr vertical={vertical} {...props} />;
+  const styles = {
+    vertical: {
+      display: "inline-block",
+      marginBlockStart: "auto",
+      marginBlockEnd: "auto",
+      marginInline: "0.5em",
+      height: 50,
+    },
+  };
+  return <hr {...props} style={styles.vertical} />;
 }
 
 hr.propTypes = {
