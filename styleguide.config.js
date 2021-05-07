3924fdf548056ccad82683fa86659344eb59475b
BossBele <adambeleko@gmail.com>
2021-05-07 18:31:40 +0300

    config

--- styleguide.config.js
+++ styleguide.config.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 
 module.exports = {
   components: () => {
-    return glob.sync("src/shared/components/**/*.{ts,tsx}").filter((file) => {
+    return glob.sync("components/**/*.{ts,tsx}").filter((file) => {
       // Take only connect component if exists, ignore others.
       if (file.match(/connect.tsx$/)) {
         return true;
