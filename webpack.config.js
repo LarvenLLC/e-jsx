51e0ab36b0ba309e2cb67ab04e98344322841733
BossBele <adambeleko@gmail.com>
2021-05-07 18:31:06 +0300

    config

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,12 @@ module.exports = {
   //   },
 
   // Enable sourcemaps for debugging webpack"s output.
-  devtool: "source-map",
+  //   devtool: "source-map",
 
-  resolve: {
-    // Add ".ts" and ".tsx" as resolvable extensions.
-    extensions: [".ts", ".tsx", ".js", ".json"],
-  },
+  //   resolve: {
+  //     // Add ".ts" and ".tsx" as resolvable extensions.
+  //     extensions: [".ts", ".tsx", ".js", ".json"],
+  //   },
 
   module: {
     rules: [
