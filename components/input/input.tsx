5819f07af66b68fa1b180a8fd688934397a8b838
BossBele <adambeleko@gmail.com>
2021-05-09 10:40:39 +0300

    ui

--- components/input/input.tsx
+++ components/input/input.tsx
@@ -1,17 +1,80 @@
 import * as React from "react";
 
 type Props = {
+  append: string;
   format: string;
-  buttonLeft: string;
-  buttonRight: string;
   onChange: (event: React.ChangeEvent<HTMLInputElement>, value: string) => void;
+  prepend: string;
 };
 
-export default function Input({ format, onChange, ...props }: Props) {
+export default function Input({
+  append,
+  format,
+  onChange,
+  prepend,
+  ...props
+}: Props) {
   return (
-    <input
-      {...props}
-      onChange={(event) => onChange(event, event.target.value)}
-    />
+    <InputContainer append={append} prepend={prepend}>
+      <input
+        {...props}
+        onChange={(event) => onChange(event, event.target.value)}
+      />
+    </InputContainer>
   );
 }
+
+type ContainerProps = {
+  append: any;
+  appendStyle: object;
+  children: JSX.Element;
+  prepend: any;
+};
+
+function InputContainer({
+  append,
+  appendStyle,
+  prepend,
+  children,
+}: ContainerProps) {
+  function Append() {
+    if (append) {
+      return <div style={{ ...styles.append, ...appendStyle }}>{append}</div>;
+    }
+    return null;
+  }
+
+  function Prepend() {
+    if (prepend) {
+      return <div style={{ ...styles.append, ...appendStyle }}>{prepend}</div>;
+    }
+    return null;
+  }
+
+  if (append || prepend) {
+    return (
+      <div style={styles.group}>
+        <Prepend />
+        {children}
+        <Append />
+      </div>
+    );
+  }
+  return children;
+}
+
+const styles = {
+  group: {
+    position: "relative",
+    display: "flex",
+    flexWrap: "wrap",
+    alignItems: "stretch",
+    width: "100%",
+  },
+  append: {
+    display: "flex",
+    alignItems: "center",
+    textAlign: "center",
+    whiteSpace: "nowrap",
+  },
+};
